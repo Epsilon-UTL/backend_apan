5195e64cfa3d818e94eedea5bb7577aad4464ed1
Angel <angelgutierrez.yuki@gmail.com>
2025-03-19 15:05:21 -0600

    19/03/2025. Angel. Inicio de downgrade

--- public/js/dashboard/app.js
+++
@@ -1,6 +0,0 @@
-alert(12);
-Echo.private('user.' + userId)
-    .listen('NotificationEvent', (event) => {
-        console.log(event.message); // Puedes mostrar la notificación aquí
-        alert(event.message);  // Mostrar una alerta, o actualizar el contador de notificaciones
-    });
--- resources/js/app.js
+++
@@ -1,7 +0,0 @@
-import './bootstrap';
-
-Echo.private('user.' + userId)
-    .listen('NotificationEvent', (event) => {
-        console.log(event.message); // Puedes mostrar la notificación aquí
-        alert(event.message);  // Mostrar una alerta, o actualizar el contador de notificaciones
-    });
